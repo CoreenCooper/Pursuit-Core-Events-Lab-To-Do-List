38ef2af9686eb66639b868d3f30bc635339eef3e
Coreen Cooper <coreencooper@pursuit.org>
2021-02-25 12:31:09 -0500

    strike out item on todo list

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 
 const form = document.querySelector("form");
 const ul = document.querySelector("ul");
+const p = document.querySelector("p");
 
 const toDolist = (event) => {
     event.preventDefault();
     const userInput = document.querySelector("input").value; // why does this have to be inside the function?
     const li = document.createElement("li");
-    li.textContent = userInput;
-    ul.appendChild(li);
+    if (userInput === "") {
+        p.textContent = "please enter string" 
+    } else {
+        li.textContent = userInput;
+        ul.appendChild(li);
+        p.textContent = "";
+    }
+
 }
+const strikeOut = (event) => {
+    event.target.style.textDecoration = "line-through";
+}
+
+ul.addEventListener("click", strikeOut)
 
 form.addEventListener("submit", toDolist) 
 

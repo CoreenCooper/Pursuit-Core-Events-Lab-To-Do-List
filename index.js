e361404c4ca6ac51070bc80fd0fe07f4f7889486
Coreen Cooper <coreencooper@pursuit.org>
2021-02-25 11:34:09 -0500

    append li to ul

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Code your solution!")
+
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+
+const toDolist = (event) => {
+    event.preventDefault();
+    const userInput = document.querySelector("input").value; // why does this have to be inside the function?
+    const li = document.createElement("li");
+    li.textContent = userInput;
+    ul.appendChild(li);
+}
+
+form.addEventListener("submit", toDolist) 
+

e99304128e680cb64d71c45195a04d13220f2376
Coreen Cooper <coreencooper@pursuit.org>
2021-03-25 21:31:20 -0400

    add list items

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 // When the user writes something in the `form`'s text input area and clicks `submit`,
 // the `ul` should update with a new `li` item at the bottom of the list. The page **should not refresh**.
-  
+
 //     1. Add an event listener to the form with `.addEventListener`. What event do you want to listen for?
 //     2. Remember, what does `event.preventDefault()` do?
 //     3. Grab the value the user typed from the text input. Do you remember what property of the input node has this?
 //     4. Create new `li` element with `document.createElement()`. Set its `textContent` property to be the text the user typed.
 //     5. Don't forget to append the created `li` to the list.
 
+const addItems = (e) => {
+  e.preventDefault();
+  const userInput = document.querySelector("#user-input");
+  const list = document.querySelector("ul");
+  const listItem = document.createElement("li");
+  listItem.textContent = userInput.value;
+  list.appendChild(listItem);
+};
+
+const form = document.querySelector("form");
+form.addEventListener("submit", addItems);
+
 // When the user writes _nothing_ in the `form`'s text input area and clicks `submit`, an error message (inside a `p` tag) should appear below the form.
 
 //     1. How can you check if the input text has something typed or not?
-//     2. Have an empty paragraph that is above the `<ul>` and under the `<form>`. If the user didn't type anything, modify the content of the paragraph to display a text like: 'Error. Todo cannot be empty' 
+//     2. Have an empty paragraph that is above the `<ul>` and under the `<form>`. If the user didn't type anything, modify the content of the paragraph to display a text like: 'Error. Todo cannot be empty'
 
 // When the user clicks on one of the `li` items, the item should be crossed out, indicating that that to-do is complete. You will need to look at [`[element].style.textDecoration`](https://www.w3schools.com/jsref/prop_style_textdecoration.asp) for the cross out effect. Look at all the different text decoration options.
 
@@ -21,6 +33,6 @@
 // ## Bonus Tasks
 // - Have the input go back to empty after adding a new todo.
 // - Implement a delete `button` next to each `li` that removes that `li` tag entirely.
-// - Clicking a todo that is crossed out (completed) uncrosses the todo.  
+// - Clicking a todo that is crossed out (completed) uncrosses the todo.
 // - Add the ability to add multiple to-dos if the user submits a text input with multiple lines. Each line should be a new to-do.
 // - Add some CSS styling to your app.
